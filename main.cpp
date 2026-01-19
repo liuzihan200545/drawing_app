693c70d185fa4db2abf50c79a9db1e1733aa8e17
liuzihan200545 <3069522719@qq.com>
2026-01-19 19:19:36 +0800

    改进了线绘制算法，添加了保存功能

--- main.cpp
+++ main.cpp
@@ -16,6 +16,11 @@ uint32_t color_palette[] = {0x000000,0xffffff,0xff0000,0x00ff00,0x0000ff,0x00fff
 uint32_t color = START_COLOR;
 int brush_size = START_RADIUS;
 
+bool is_pressed = false;
+
+int x_last = 0;
+int y_last = 0;
+
 void draw_palette(SDL_Surface* surface, const uint32_t* colors, size_t size)
 {
     SDL_Rect color_rect;
@@ -23,23 +28,6 @@ void draw_palette(SDL_Surface* surface, const uint32_t* colors, size_t size)
     {
         color_rect = {static_cast<int>(i*COLOR_RECT_SIZE),0,COLOR_RECT_SIZE,COLOR_RECT_SIZE};
         SDL_FillRect(surface,&color_rect,colors[i]);
-    }
-}
-
-void draw_circle(SDL_Surface* surface, int x, int y, int radius, uint32_t color)
-{
-    SDL_Rect pixel = SDL_Rect{0,0,1,1};
-    for (int i = y-radius; i < y+radius; ++i)
-    {
-        for (int j = x-radius; j < x+radius; ++j)
-        {
-            if (pow((y-i),2)+pow((x-j),2)<=pow(radius,2))
-            {
-                pixel.x = j;
-                pixel.y = i;
-                SDL_FillRect(surface,&pixel,color);
-            }
-        }
     }
 }
 
@@ -51,6 +39,26 @@ bool check_position_color_palette(int x, int y)
         return true;
     }
     return false;
+}
+
+void draw_circle(SDL_Surface* surface, int x, int y, int radius, uint32_t color)
+{
+    if (!check_position_color_palette(x,y))
+    {
+        SDL_Rect pixel = SDL_Rect{0,0,1,1};
+        for (int i = y-radius; i < y+radius; ++i)
+        {
+            for (int j = x-radius; j < x+radius; ++j)
+            {
+                if (pow((y-i),2)+pow((x-j),2)<=pow(radius,2))
+                {
+                    pixel.x = j;
+                    pixel.y = i;
+                    SDL_FillRect(surface,&pixel,color);
+                }
+            }
+        }
+    }
 }
 
 void check_color_palette_chosen(int x,int y)
@@ -68,6 +76,20 @@ void clean_screen(SDL_Window* window, SDL_Surface* surface,uint32_t clean_color)
     SDL_UpdateWindowSurface(window);
     draw_palette(surface,color_palette,std::size(color_palette));
     SDL_UpdateWindowSurface(window);
+}
+
+void bresenham(SDL_Surface* surface, int x0, int y0, int x1, int y1) {
+    int dx = abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
+    int dy = abs(y1 - y0), sy = y0 < y1 ? 1 : -1;
+    int err = (dx > dy ? dx : -dy) / 2, e2;
+
+    while (true) {
+        draw_circle(surface,x0,y0,brush_size,color);
+        if (x0 == x1 && y0 == y1) break;
+        e2 = err;
+        if (e2 > -dx) { err -= dy; x0 += sx; }
+        if (e2 < dy) { err += dx; y0 += sy; }
+    }
 }
 
 int main(void)
@@ -103,36 +125,48 @@ int main(void)
                     x = event.motion.x;
                     y = event.motion.y;
                     check_color_palette_chosen(x,y);
-                    draw = !check_position_color_palette(x,y);
+                    draw_circle(surface,x,y,brush_size,color);
+                    is_pressed = true;
+                    x_last = x;
+                    y_last = y;
                     break;
                 case SDL_MOUSEBUTTONUP:
-                    draw = false;
+                    is_pressed = false;
                     break;
                 case SDL_MOUSEMOTION:
                     x = event.motion.x;
                     y = event.motion.y;
+                    if (is_pressed)
+                    {
+                        bresenham(surface,x,y,x_last,y_last);
+                    }
+                    x_last = x;
+                    y_last = y;
                     break;
                 case SDL_MOUSEWHEEL:
                     brush_size = std::max(brush_size, 4);
                     brush_size += event.wheel.y;
+                    break;
                 case SDL_KEYDOWN:
                     if (event.key.keysym.sym == SDLK_TAB)
                     {
                         clean_screen(window,surface,CLEAN_COLOR);
+                    }
+                    else if (event.key.keysym.sym == SDLK_s)
+                    {
+                        SDL_SaveBMP(surface, "paint.bmp");
+                        printf("The painting has been saved as paint.bmp\n");
                     }
                     break;
                 default: ;
             }
         }
 
-        if (draw)
-        {
-            draw_circle(surface,x,y,brush_size,color);
-            draw_palette(surface,color_palette,std::size(color_palette));
-            SDL_UpdateWindowSurface(window);
-        }
+        draw_palette(surface,color_palette,std::size(color_palette));
+        SDL_UpdateWindowSurface(window);
         
         SDL_Delay(static_cast<uint32_t>(delay_millis));
+        
     }
     SDL_DestroyWindow(window);
     SDL_Quit();
